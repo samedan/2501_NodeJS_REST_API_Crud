fb1ff8c84cce30bfb25d83941a0fc70b02566225
Popescu Daniel <same.dan@gmail.com>
2025-01-05 11:03:11 +0100

    Delete User

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
+let users = [
   {
     firstName: "John",
     lastName: "Doe",
@@ -21,11 +22,27 @@ router.get("/", (req, res) => {
   res.send(users);
 });
 
+// @ GET /users/:id
+router.get("/:id", (req, res) => {
+  let id = req.params.id;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
 // @ POST /users
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
+  const userId = uuidv4();
+  const userWithId = { ...user, id: userId };
+  users.push(userWithId);
   res.send(`User with the name ${user.firstName} added to the DBB`);
+});
+
+// @ DELETE /users/:id
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`User with id: ${id} deleted from DBB`);
 });
 
 export default router;
